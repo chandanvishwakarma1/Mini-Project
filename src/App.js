aab0ba9c34f22cdce005e870d6aa37cf06ef9deb
chandanvishwakarma1 <vishwakarmachandan2005@gmail.com>
2025-02-15 15:05:28 +0530

    Updated landing page and added features

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import LandingPage from './components/LandingPage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+// src/components/Navbar.js
+import React from 'react';
+import './Navbar.css';
+import logo from './logo.png'; // Ensure you have a logo image named "logo.png" in the "src/components" folder
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>T&P Portal</h1>
+        <ul className="navbar-options">
+          <li><a href="#communities">Communities</a></li>
+          <li><a href="#stats">Stats</a></li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <button className="login-button">Login</button>
+        <button className="signup-button">Sign Up</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
